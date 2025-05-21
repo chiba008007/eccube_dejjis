b5fddd2354306b5f28468988064260c67bfdf26f
t.chiba <takahiro_chiba@dejjis.co.jp>
2025-05-21 16:04:45 +0900

    mockサーバーの追加

--- e2e/server/server.js
+++ e2e/server/server.js
@@ -7,6 +7,9 @@ const path = require('path');
 const PORT = 3456;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> be38eb2 (mockサーバーの追加)
 // const requestXml = fs.readFileSync(path.join(__dirname, 'mockdata/mock-cxml-api-request.xml'), 'utf-8');
 // const responseXml = fs.readFileSync(path.join(__dirname, 'mockdata/mock-cxml-api-response.xml'), 'utf-8');
 
@@ -20,14 +23,18 @@ const responseMap = {
     contentType: 'application/xml',
     file: 'mock-cxml-api-response-PunchoutOrderMessage.xml'
   },
+<<<<<<< HEAD
   '/punchOutSetupRequestPunchoutOrderMessage': {
     contentType: 'application/xml',
     file: 'mock-cxml-api-response-PunchoutOrderMessage.xml'
   },
+=======
+>>>>>>> be38eb2 (mockサーバーの追加)
   '/amazonJsonApiSample': {
     contentType: 'application/json',
     file: 'mock-json-api-response.json'
   },
+<<<<<<< HEAD
 
 };
 
@@ -55,6 +62,29 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/xml' });
     res.end(responseXml);
 >>>>>>> b6c0549 (mock用サーバー追加)
+=======
+  // '/amazonErrorApiSample': {
+  //   contentType: 'application/xml',
+  //   file: 'mock-error-api-response.xml'
+  // }
+};
+
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && responseMap[req.url] ) {
+    const { contentType, file } = responseMap[req.url];
+    const filePath = path.join(__dirname, 'mockdata', file);
+
+    try{
+      const responseData = fs.readFileSync(filePath, 'utf-8');
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(responseData);
+    }catch(err){
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end( 'Internal Server Error' );
+    }
+
+>>>>>>> be38eb2 (mockサーバーの追加)
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
